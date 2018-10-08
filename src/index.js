ebabaf33fbec4e2abe54b80ce66e500943e5ae8d
michael.rasskazov <misharasskazov1@gmail.com>
2018-10-08 20:27:30 +0400

    smart-calculator-version1

--- src/index.js
+++ src/index.js
@@ -1,27 +1,88 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+constructor(initialValue)  {
+return this.addOperation({
+operator: '+',
+number,
+});
+}
 
   add(number) {
-    // your implementation
-  }
+return this.addOperation({
+operator: '+',
+number,
+});
+}
   
   subtract(number) {
-    // your implementation
-  }
+return this.addOperation({
+operator: '-',
+number,
+});
+}
 
   multiply(number) {
-    // your implementation
-  }
+return this.addOperation({
+operator: '*',
+number,
+});
+}
 
   devide(number) {
-    // your implementation
-  }
-
-  pow(number) {
-    // your implementation
-  }
+return this.addOperation({
+operator: '/',
+number,
+});
 }
+
+
+
+  pow(number)  {
+return this.addOperation({
+operator: '*',
+number,
+});
+}
+addOperation(operation) {
+this.operations.push(operation);
+return this;
+}
+calculate(operations) {
+const ops = ['^', '*', '/', '+', '-'];
+let results = operations.map(o => o.number);
+ops.map(o => {
+operations.map((operation, index) => {
+if (operation.operator !== o) {
+return;
+}
+switch (operation.operator) {
+case '^':
+results[index - 1] =
+Math.pow(+results[index - 1], +operation.number);
+results[index] = 0;
+break;
+case '*':
+results[index - 1] = +results[index - 1] * +operation.number;
+results[index] = 0;
+break;
+case '/':
+results[index - 1] = +results[index - 1] / +operation.number;
+results[index] = 0;
+break;
+case '-':
+results[index] = - +operation.number;
+break;
+default:
+break;
+}
+});
+});
+return results.reduce((a,b) => a + b);
+}
+
+valueOf() {
+return this.calculate(this.operations);
+}
+}
+
 
 module.exports = SmartCalculator;
